01fb5242764a8fff1644a791397c54126b093ae6
sqwk <suponinlesha@gmail.com>
2023-03-31 22:33:34 +0300

    Add Fragments and life circles

---
+++ app/src/main/java/com/example/parkme/SignUpFragment.java
@@ -0,0 +1,100 @@
+package com.example.parkme;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+public class SignUpFragment extends Fragment {
+    private static final String TAG = "SignUpFragment";
+
+    public SignUpFragment(){
+        super(R.layout.fragment_sign_up);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach");
+        Toast.makeText(context, "Fragment attached", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        Toast.makeText(getActivity(), "Fragment created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+        Toast.makeText(getActivity(), "View created", Toast.LENGTH_SHORT).show();
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated");
+        Toast.makeText(getActivity(), "View created and attached to fragment", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        Toast.makeText(getActivity(), "Fragment started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        Toast.makeText(getActivity(), "Fragment resumed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        Toast.makeText(getActivity(), "Fragment paused", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        Toast.makeText(getActivity(), "Fragment stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView");
+        Toast.makeText(getActivity(), "View destroyed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        Toast.makeText(getActivity(), "Fragment destroyed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach");
+        Toast.makeText(getActivity(), "Fragment detached", Toast.LENGTH_SHORT).show();
+    }
+}
